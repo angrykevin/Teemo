6019831d9b96867b6d027f2689e4b32e82e4c5bb
Kevin <hpwasy@gmail.com>
2013-11-06 16:55:21 +0800

    blah blah

--- Teemo/RSSigninViewController.m
+++ Teemo/RSSigninViewController.m
@@ -23,10 +23,68 @@
 {
   [super viewDidLoad];
   
-//  _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
-//  _tableView.dataSource = self;
-//  _tableView.delegate = self;
+  _navigationView.leftButton.hidden = YES;
+  _navigationView.rightButton.hidden = YES;
+  _navigationView.titleLabel.text = NSLocalizedString(@"登录", @"");
   
+  _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+  _tableView.dataSource = self;
+  _tableView.delegate = self;
+  [_contentView addSubview:_tableView];
+  
+}
+
+- (void)layoutViews
+{
+  [super layoutViews];
+  
+  _tableView.frame = _contentView.bounds;
+  
+}
+
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+  return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+  return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  UITableViewCell *cell = [tableView dequeueReusableCellWithClass:[UITableViewCell class]];
+  return cell;
+}
+
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+  return 150.0;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+  UIView *header = [[UIView alloc] init];
+  header.backgroundColor = [UIColor redColor];
+  return header;
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+  return 100.0;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+  UIView *v = [[UIView alloc] init];
+  v.backgroundColor = [UIColor blueColor];
+  return v;
 }
 
 @end
